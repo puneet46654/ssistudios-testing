ab70820aa42663d5510721c515d158ce43ae1020
Puneet Shukla <puneet.shukla@ssinnovations.org>
2026-09-10 09:22:20 +0530

    .

--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardClient from '@/components/dashboard/DashboardClient';
-
-export default function Page() {
-  return <DashboardClient />;
-}
--- components/security-guard.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function SecurityGuard() {
-  return null;
-}
